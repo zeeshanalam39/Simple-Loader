f8a85e44058cf86067eeede56180977aa78c3b93
Zeeshan Alam <zeeshanalam285@gmail.com>
2020-07-14 20:50:55 +0500

    Added Start/Stop Loader Feature!

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let content = document.getElementsByTagName('h2')[0];
 let loader = document.querySelector('.loader');
+<<<<<<< HEAD
 content.style.display = 'none'; // Hide website content at start.
 
 setTimeout(showContent, 3000); // Show Loader for 3s & then show website content.
@@ -8,3 +9,29 @@ function showContent() {
     content.style.display = 'block';
     loader.remove() // Remove loader after content got loaded.
 }
+=======
+let startBtn = document.querySelector('#start');
+let stopBtn = document.querySelector('#stop');
+
+content.style.display = 'none'; // Hide website content at start.
+
+setTimeout(showContent, 5000); // Show Loader for 5s & then show website content.
+
+function showContent() {
+    content.style.display = 'block';
+    loader.style.display = 'none';
+}
+
+function startStop(event) {
+    if (event.target.id === 'start') {
+        content.style.display = 'none';
+        loader.style.display = 'block';
+    } else {
+        content.style.display = 'block';
+        loader.style.display = 'none';
+    }
+}
+
+startBtn.addEventListener('click', startStop); // Start loading
+stopBtn.addEventListener('click', startStop); // Stop loading
+>>>>>>> Added Start/Stop Loader Feature!
